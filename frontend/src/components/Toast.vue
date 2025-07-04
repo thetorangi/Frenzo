6d48d1ffc9dba6c54f7fca4b0cf11d8967edb77a
K A R M A ☯️ <torangisb@gmail.com>
2025-07-04 18:14:09 +0000

    Added Components

---
+++ frontend/src/components/Toast.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { useToastStore } from '@/stores/toast'
+
+const store = useToastStore()
+
+return {
+    store
+}
+</script>
+
+<template>
+
+    <div
+  v-if="store.isVisible"
+  class="fixed right-4 bottom-4 sm:right-8 sm:bottom-8 px-6 py-6 rounded-xl shadow-xl z-50
+         transition-all ease-in-out duration-500
+         bg-white text-gray-800 dark:bg-gray-800 dark:text-white"
+  :class="store.classes"
+>
+  <!-- Your content here -->
+   {{ store.message }}
+</div>
+
+</template>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,11 +6,22 @@ import axios from 'axios'
 import App from './App.vue'
 import router from './router'
 
+// Import global components
+import PeopleYouMayKnow from './components/PeopleYouMayKnow.vue'
+import Trends from './components/Trends.vue'
+
 const app = createApp(App)
 
 // Set up axios base URL
 axios.defaults.baseURL = 'http://localhost:8000'
 
+
+// Register global components 
+app.component('PeopleYouMayKnow', PeopleYouMayKnow)
+app.component('Trends', Trends)
+
+
+// Set up axios as a global property
 app.use(createPinia())
 app.use(router)
 
