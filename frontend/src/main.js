94fbc3f39cae1341aa9b88a193039638b90a8c07
K A R M A ☯️ <torangisb@gmail.com>
2025-07-04 17:16:08 +0000

    Axios Path Added

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,11 +2,14 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import axios from 'axios'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+
+// Set up axios base URL
+axios.defaults.baseURL = 'http://localhost:8000'
 
 app.use(createPinia())
 app.use(router)
