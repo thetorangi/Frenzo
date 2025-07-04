ad118b28a1a423e6fd7cf2180f778958914a664a
K A R M A ☯️ <torangisb@gmail.com>
2025-07-04 17:27:19 +0000

    Yeah Tailwind Works

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,5 @@
 <template>
-  <p class="text-center text-2xl font-bold mt-10">
-    Welcome to Frenzo!
-  </p>
+  <h1 class="text-center text-2xl font-bold mt-10">Welcome to Frenzo!</h1>
 
   <RouterView />
 </template>
