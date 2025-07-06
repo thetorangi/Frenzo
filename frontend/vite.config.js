aae39b876a175aa5631cfdfbdbf6a6b689f7f9f3
K A R M A ☯️ <torangisb@gmail.com>
2025-07-06 04:37:39 +0000

    Version Change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,19 +3,17 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    tailwindcss(),
     vueDevTools(),
   ],
   darkMode: 'class',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+    }
+  }
 })
