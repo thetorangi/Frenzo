cf924d6c9a17bfc3aab392ab77be2e2b94615181
K A R M A ☯️ <torangisb@gmail.com>
2025-07-07 16:35:14 +0000

    Updated Account Model

--- backend/account/models.py
+++ backend/account/models.py
@@ -1,11 +1,6 @@
+import uuid 
 
-import uuid #uuid is used to generate unique identifiers for the User model
-
-# backend/account/models.py
-# This file defines a custom user model for the Django application, which includes fields for email,
-# name, avatar, and various user status flags. It also includes a custom user manager for creating users and superusers.
-# The custom user model extends Django's AbstractBaseUser and PermissionsMixin
-# to provide authentication and permission handling capabilities.
+from django.conf import settings
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
 from django.db import models
 from django.utils import timezone
@@ -16,8 +11,7 @@ class CustomUserManager(UserManager):
     def _create_user(self, name, email, password, **extra_fields):
         if not email:
             raise ValueError("You have not provided a valid e-mail address")
-        if name is None:
-            name = email.split('@')[0]  # Default name to the part before '@' if not provided
+        
         email = self.normalize_email(email)
         user = self.model(email=email, name=name, **extra_fields)
         user.set_password(password)
@@ -41,6 +35,12 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=255, blank=True, default='')
     avatar = models.ImageField(upload_to='avatars', blank=True, null=True)
+    friends = models.ManyToManyField('self')
+    friends_count = models.IntegerField(default=0)
+
+    people_you_may_know = models.ManyToManyField('self')
+
+    posts_count = models.IntegerField(default=0)
 
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
@@ -54,3 +54,27 @@ class User(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = 'email'
     EMAIL_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    def get_avatar(self):
+        if self.avatar:
+            return settings.WEBSITE_URL + self.avatar.url
+        else:
+            return 'https://picsum.photos/200/200'
+
+
+class FriendshipRequest(models.Model):
+    SENT = 'sent'
+    ACCEPTED = 'accepted'
+    REJECTED = 'rejected'
+
+    STATUS_CHOICES = (
+        (SENT, 'Sent'),
+        (ACCEPTED, 'Accepted'),
+        (REJECTED, 'Rejected'),
+    )
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_for = models.ForeignKey(User, related_name='received_friendshiprequests', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, related_name='created_friendshiprequests', on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=SENT)
