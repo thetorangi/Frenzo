35bcc5007cb2e7517bd512d7187f8b5a9ab1b229
K A R M A ☯️ <torangisb@gmail.com>
2025-07-22 12:38:12 +0000

    Added Routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,10 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import SignUpView  from '../views/SignUpView.vue'
-import LoginView from '@/views/LoginView.vue'
-import FeedView from '@/views/FeedView.vue'
-import MessageView from '@/views/MessageView.vue'
-import SearchView from '@/views/SearchView.vue'
+import FeedView from '../views/FeedView.vue'
+import SignupView from '../views/SignupView.vue'
+import LoginView from '../views/LoginView.vue'
+import SearchView from '../views/SearchView.vue'
+import ProfileView from '../views/ProfileView.vue'
+import FriendsView from '../views/FriendsView.vue'
+import PostView from '../views/PostView.vue'
+import ChatView from '../views/ChatView.vue'
+import TrendView from '../views/TrendView.vue'
+import EditProfileView from '../views/EditProfileView.vue'
+import EditPasswordView from '../views/EditPasswordView.vue'
+import NotificationsView from '../views/NotificationsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,32 +19,67 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/signup',
-      name: 'signup',
-      component: SignUpView,
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: LoginView,
+      component: HomeView
     },
     {
       path: '/feed',
       name: 'feed',
-      component: FeedView,
+      component: FeedView
     },
     {
-      path: '/messages',
-      name: 'messages',
-      component: MessageView,
+      path: '/signup',
+      name: 'signup',
+      component: SignupView
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView
     },
     {
       path: '/search',
       name: 'search',
-      component: SearchView,
+      component: SearchView
+    },
+    {
+      path: '/chat',
+      name: 'chat',
+      component: ChatView
+    },
+    {
+      path: '/notifications',
+      name: 'notifications',
+      component: NotificationsView
+    },
+    {
+      path: '/profile/edit',
+      name: 'editprofile',
+      component: EditProfileView
+    },
+    {
+      path: '/profile/edit/password',
+      name: 'editpassword',
+      component: EditPasswordView
+    },
+    {
+      path: '/profile/:id',
+      name: 'profile',
+      component: ProfileView
+    },
+    {
+      path: '/profile/:id/friends',
+      name: 'friends',
+      component: FriendsView
+    },
+    {
+      path: '/:id',
+      name: 'postview',
+      component: PostView
+    },
+    {
+      path: '/trends/:id',
+      name: 'trendview',
+      component: TrendView
     },
     {
       path: '/about',
@@ -45,9 +87,9 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
+      component: () => import('../views/AboutView.vue')
+    }
+  ]
 })
 
 export default router
