e4c7ab8b3657d439a145742fbf30d0ae6fbe9949
K A R M A ☯️ <torangisb@gmail.com>
2025-07-22 13:06:36 +0000

    Updated Components

--- frontend/src/components/Toast.vue
+++ frontend/src/components/Toast.vue
@@ -1,27 +1,3 @@
-<!-- <script setup>
-import { useToastStore } from '@/stores/toast'
-
-const store = useToastStore()
-
-
-</script>
-
-<template>
-
-    <div
-  v-if="store.isVisible"
-  class="fixed right-4 bottom-4 sm:right-8 sm:bottom-8 px-6 py-6 rounded-xl shadow-xl z-50
-         transition-all ease-in-out duration-500
-         bg-white text-gray-800 dark:bg-gray-800 dark:text-white"
-  :class="store.classes"
->
-  
-   {{ store.message }}
-</div>
-
-</template> -->
-
-
 <script setup>
 import { useToastStore } from '@/stores/toast'
 
--- frontend/src/components/Trends.vue
+++ frontend/src/components/Trends.vue
@@ -1,48 +1,56 @@
 <template>
-    <div class="p-6 bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-2xl shadow-md">
-          <h3 class="text-lg font-semibold mb-4">Trends</h3>
-          <div class="space-y-4 text-sm text-gray-600 dark:text-gray-300">
-            
-              <div class="flex items-center justify-between">
-                <div>
-                  <p><strong>#coderKarma</strong></p>
-                  <p class="text-xs text-gray-400">180 posts</p>
-                </div>
-                <button class="py-1 px-3 bg-purple-600 hover:bg-purple-700 text-white text-xs rounded-lg transition">Explore</button>
-              </div>
+  <div class="p-4 bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-700 rounded-lg shadow transition-colors duration-300">
+    <h3 class="mb-6 text-xl font-semibold text-gray-800 dark:text-white">Trends</h3>
 
-              <div class="flex items-center justify-between">
-                <div>
-                  <p><strong>#coderKarma</strong></p>
-                  <p class="text-xs text-gray-400">180 posts</p>
-                </div>
-                <button class="py-1 px-3 bg-purple-600 hover:bg-purple-700 text-white text-xs rounded-lg transition">Explore</button>
-              </div>
+    <div class="space-y-4">
+      <div
+        class="flex items-center justify-between hover:bg-gray-100 dark:hover:bg-gray-800 p-2 rounded transition-colors"
+        v-for="trend in trends"
+        :key="trend.id"
+      >
+        <p class="text-xs text-gray-800 dark:text-gray-300">
+          <strong>#{{ trend.hashtag }}</strong><br />
+          <span class="text-gray-500 dark:text-gray-400">{{ trend.occurences }} posts</span>
+        </p>
 
-              <div class="flex items-center justify-between">
-                <div>
-                  <p><strong>#coderKarma</strong></p>
-                  <p class="text-xs text-gray-400">180 posts</p>
-                </div>
-                <button class="py-1 px-3 bg-purple-600 hover:bg-purple-700 text-white text-xs rounded-lg transition">Explore</button>
-              </div>
-
-              <div class="flex items-center justify-between">
-                <div>
-                  <p><strong>#coderKarma</strong></p>
-                  <p class="text-xs text-gray-400">180 posts</p>
-                </div>
-                <button class="py-1 px-3 bg-purple-600 hover:bg-purple-700 text-white text-xs rounded-lg transition">Explore</button>
-              </div>
-
-              <div class="flex items-center justify-between">
-                <div>
-                  <p><strong>#coderKarma</strong></p>
-                  <p class="text-xs text-gray-400">180 posts</p>
-                </div>
-                <button class="py-1 px-3 bg-purple-600 hover:bg-purple-700 text-white text-xs rounded-lg transition">Explore</button>
-              </div>
-            
-          </div>
-        </div>
+        <RouterLink
+          :to="{ name: 'trendview', params: { id: trend.hashtag } }"
+          class="py-2 px-3 bg-purple-600 hover:bg-purple-700 transition text-white text-xs rounded-lg shadow"
+        >
+          Explore
+        </RouterLink>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Trends',
+
+  data() {
+    return {
+      trends: []
+    }
+  },
+
+  mounted() {
+    this.getTrends()
+  },
+
+  methods: {
+    getTrends() {
+      axios
+        .get('/api/posts/trends/')
+        .then(response => {
+          this.trends = response.data
+        })
+        .catch(error => {
+          console.error('Error:', error)
+        })
+    }
+  }
+}
+</script>
