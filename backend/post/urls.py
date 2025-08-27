1b247bb192059393bfb4b4945447066ea35e55ed
K A R M A ☯️ <torangisb@gmail.com>
2025-08-27 18:26:27 +0530

    url for unlike post urls.py

--- backend/post/urls.py
+++ backend/post/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', api.post_list, name='post_list'),
     path('<uuid:pk>/', api.post_detail, name='post_detail'),
     path('<uuid:pk>/like/', api.post_like, name='post_like'),
+    path('<uuid:pk>/unlike/', api.post_unlike, name='post_unlike'),
     path('<uuid:pk>/comment/', api.post_create_comment, name='post_create_comment'),
     path('<uuid:pk>/delete/', api.post_delete, name='post_delete'),
     path('<uuid:pk>/report/', api.post_report, name='post_report'),
