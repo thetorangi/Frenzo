c07de00f4ef6d2c95401ed274cb099f47853b34f
K A R M A ☯️ <torangisb@gmail.com>
2025-08-28 00:01:40 +0530

    added delete comment urls.py

--- backend/post/urls.py
+++ backend/post/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('<uuid:pk>/unlike/', api.post_unlike, name='post_unlike'),
     path('<uuid:pk>/comment/', api.post_create_comment, name='post_create_comment'),
     path('<uuid:pk>/delete/', api.post_delete, name='post_delete'),
+    path('<uuid:pk>/com_delete/', api.comment_delete, name='comment_delete'),
     path('<uuid:pk>/report/', api.post_report, name='post_report'),
     path('profile/<uuid:id>/', api.post_list_profile, name='post_list_profile'),
     path('create/', api.post_create, name='post_create'),
